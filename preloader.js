a6f68492c67e8847019287306f3b5c9207bf1b2b
Tsutomu Morishita <tom@cnaps.co>
2024-01-20 21:35:49 +0900

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://cdn.jsdelivr.net/gh/jasminder/flutter_preloader/preload.svg';
+svgImage.src = 'https://cdn.jsdelivr.net/gh/tstomtimes/flutter_preloader@main/preload.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;
